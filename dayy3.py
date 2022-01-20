1a8c4f648a3f5a4e0598efbf12cd7016da418fad
ajaycu123 <98098234+ajaycu123@users.noreply.github.com>
2022-01-20 18:56:31 +0530

    day 3 assigment

---
+++ dayy3.py
@@ -0,0 +1,9 @@
+# Python3 program introducing f-string
+
+name = 'Ajay'
+age = 25
+qualification ='btech'
+address ='india'
+
+print(f"Hello, My name is {name} and I'm {age} years old i live in {address} i studied {qualification}.")
+
