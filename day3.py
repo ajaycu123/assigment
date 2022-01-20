64d038688031b458547156f99179881e99e1829e
ajaycu123 <98098234+ajaycu123@users.noreply.github.com>
2022-01-20 18:24:41 +0530

    day 3 Assignment

---
+++ day3.py
@@ -0,0 +1,11 @@
+# user input
+name = input("what is your name")
+age = int (input("how old are you"))
+address = input("where are you from")
+qualification = input("what is your qualification")
+
+print("my name is "+name)
+print("iam " +str(age)+ " years old")
+print("iam from "+address)
+print("my qualification is "+qualification)
+
