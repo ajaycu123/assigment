26712bbf8995b494c252a8831e7755eebffd20a4
ajaycu123 <98098234+ajaycu123@users.noreply.github.com>
2022-01-20 18:22:07 +0530

    Add files via upload

---
+++ day 2.py
@@ -0,0 +1,54 @@
+
+
+# integer
+a = 10
+print(a)
+# addition
+print(5 + 10)
+
+
+# string
+language= "ajay"
+print(f"Hello {language} World!")
+
+
+# float
+x = 20.85669842
+y = -0.8566945
+z = 0.00000000000000000000125563
+print(x)
+print(y)
+print(z)
+
+
+#  f string
+x = 42.222222222
+print(f'My value is: {x}')
+print(f"{6+7}")
+
+# list
+lst = [10,20,30,'John',50,'Joe']
+print("Elements of the List:\n")
+print(*lst, sep = "\n")
+
+
+# tuple
+mytuple = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+# First value
+print(mytuple[0])
+# Second value
+print(mytuple[1])
+mytuple = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+print(mytuple[1:3])
+
+
+# Boolean
+x = "Hello"
+y = 15
+print(bool(x))
+print(bool(y))
+
+
+# dictionary
+Dict = {'Tim': 18,'Charlie':12,'Tiffany':22,'Robert':25}
+print (Dict['Tiffany'])
